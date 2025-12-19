d7227a99c236c3a870a378c471233216479d451f
Charan Ravanam <charanravanam5@gmail.com>
2025-12-19 12:32:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked correctly regardless of subfolder deployment
+  base: './', // Vital for GitHub Pages subfolder deployment
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "") 
   },
@@ -12,6 +13,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
   }
 })
